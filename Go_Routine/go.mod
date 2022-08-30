f0c4cff2b7078b3c6d72d73c62dcce4271dbee51
Vimal Kumar <mdjohnvimal@gmail.com>
2022-08-30 13:43:17 +0530

    Added Go routine and channel

---
+++ Go_Routine/go.mod
@@ -0,0 +1,3 @@
+module go_routine
+
+go 1.18
---
+++ Go_Routine/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+//function to print hello
+func printHello(ch chan int) {
+	fmt.Println("Hello from printHello")
+	ch <- 2
+}
+
+func main() {
+
+	//make a channel. You need to use the make function to create channels.
+	//channels can also be buffered where you can specify size. eg: ch := make(chan int, 2)
+	ch := make(chan int)
+	//inline goroutine. Define a function inline and then call it.
+	go func() {
+		fmt.Println("Hello inline")
+		ch <- 1
+	}()
+	//call a function as goroutine
+	go printHello(ch)
+	fmt.Println("Hello from main")
+
+	//get first value from channel.
+	//and assign to a variable to use this value later
+	//here that is to print it
+
+	i := <-ch
+	fmt.Println("Received ", i)
+
+	//get the second value from channel
+	//do not assign it to a variable because we dont want to use that
+
+	<-ch
+}
